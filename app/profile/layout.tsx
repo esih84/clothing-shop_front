bdc6ba0864517850c0c3b32fa3f6f71ae14d485a
e.hajiabadi <e.hajiabadi@ecoapp.ir>
2026-05-26 15:58:53 +0330

    feat: add AdminSlot component and update ProfileLayout to conditionally render admin content

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -3,15 +3,17 @@
 
 export default function ProfileLayout({
   tabs,
+  admin
 }: {
   children: React.ReactNode
   tabs: React.ReactNode
+  admin: React.ReactNode
 }) {
-
-
+  const role: string = "user"
   return (
     <div className="pt-16 pb-24 px-4 mx-auto max-w-6xl" dir="rtl">
-      {tabs}
+      {role === "user" && tabs}
+      {role === "admin" && admin}
     </div>
   )
 }
